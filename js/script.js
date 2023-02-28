b63557abb6d56c549b1ac3e05ffca25b39110359
MXerFix <maksim2003003@gmail.com>
2023-02-28 16:07:45 +0300

    img minify

--- js/script.js
+++ js/script.js
@@ -69,82 +69,90 @@ tl_1.from(".scroll-down-window", { opacity: 0, duration: 1, ease: "sine.out" })
 
 gsap.to(".typing-title", { delay: 1, duration: 5, text: { value: "Самое главное в бизнесе - сконцентрироваться <br> на создании чего-то важного." }, ease: "none" })
 
+if (widthWind > 767) {
+    gsap.from("#landing-item", {
+        x: -200,
+        // opacity:0,
+        scrollTrigger: {
+            scrub: 0,
+            start: "-1000px",
+            end: "-250px",
+            trigger: "#landing-item",
+        }
+    })
 
-// const Cards = ((() => {
-//     window.addEventListener('DOMContentLoaded', () => { setTimeout(init, 1) }, true);
+    gsap.from("#bcard-item", {
+        x: 200,
+        // opacity:0,
+        scrollTrigger: {
+            trigger: "#bcard-item",
+            scrub: 0,
+            start: "-1000px",
+            end: "-300px",
+        }
+    })
 
-//     function init(e) {
-//         if (document.querySelector(".cards")) {
-//             let cards = document.querySelector(".cards");
-//             cards.addEventListener('click', clicked, false);
-//             document.querySelectorAll(".cards .card")[1].click();
-//         }
-//     }
+    gsap.from("#multi-item", {
+        x: -200,
+        // opacity:0,
+        scrollTrigger: {
+            scrub: 0,
+            start: "-1000px",
+            end: "-450px",
+            trigger: "#multi-item",
+        }
+    })
 
-//     function clicked(e) {
-//         let card = e.target;
-//         if (card.getAttribute("data-card")) { rearrange(card.getAttribute("data-card")); }
-//     }
+    gsap.from("#shop-item", {
+        x: 200,
+        // opacity:0,
+        scrollTrigger: {
+            trigger: "#shop-item",
+            scrub: 0,
+            start: "-1000px",
+            end: "-700px",
+        }
+    })
 
-//     let buttonLeft = document.getElementById("arrow-left");
-//     buttonLeft.addEventListener("click", function () {
-//         let thatCard = document.querySelector(".card--center");
-//         let x = (thatCard.getAttribute("data-card")) - (-1);
-//         if (x > 3) { x = 0 };
-//         console.log(x)
-//         rearrange(x);
-//     })
-//     let buttonRight = document.getElementById("arrow-right");
-//     buttonRight.addEventListener("click", function () {
-//         let thatCard = document.querySelector(".card--center");
-//         let y = (thatCard.getAttribute("data-card")) - 1;
-//         if (y < 0) { y = 3 };
-//         console.log(y)
-//         rearrange(y);
-//     })
+    gsap.from(".other-service", {
+        scale: 0.3,
+        scrollTrigger: {
+            trigger: ".other-services",
+            scrub: true,
+            start: "-1000px",
+            end: "-250px"
+        }
+    })
 
-//     function rearrange(card) {
-//         let cards = document.querySelectorAll(".cards .card");
-//         for (let n = 0; n < cards.length; n++) {
-//             cards[n].classList.remove("card--left");
-//             cards[n].classList.remove("card--center");
-//             cards[n].classList.remove("card--right");
-//         }
-//         cards[card].classList.add("card--center");
-//         if (card == 0) {
-//             cards[2].classList.add("unvisible");
-//             cards[1].classList.remove("unvisible");
-//             cards[3].classList.remove("unvisible");
-//             cards[3].classList.add("card--left")
-//             cards[1].classList.add("card--right");
-//         }
-//         if (card == 1) {
-//             cards[3].classList.add("unvisible");
-//             cards[0].classList.remove("unvisible");
-//             cards[2].classList.remove("unvisible");
-//             cards[0].classList.add("card--left");
-//             cards[2].classList.add("card--right");
-//         }
-//         if (card == 2) {
-//             cards[0].classList.add("unvisible");
-//             cards[1].classList.remove("unvisible");
-//             cards[3].classList.remove("unvisible");
-//             cards[1].classList.add("card--left");
-//             cards[3].classList.add("card--right");
-//         }
-//         if (card == 3) {
-//             cards[1].classList.add("unvisible");
-//             cards[2].classList.remove("unvisible");
-//             cards[0].classList.remove("unvisible");
-//             cards[2].classList.add("card--left");
-//             cards[0].classList.add("card--right");
-//         }
-//     }
+    gsap.from(".steps-list", {
+        x: -50,
+        opacity: 0,
+        scrollTrigger: {
+            scrub: true,
+            start: "-800px",
+            end: "100%",
+            trigger: "#how-to-work",
+        }
+    })
 
-//     return {
-//         init
-//     }
-// })());
+    gsap.from(".step-icons-box", {
+        x: 50,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: "#how-to-work",
+            scrub: true,
+            start: "-800px",
+            end: "100%",
+        }
+    })
+
+
+}
+
+if (widthWind < 768) {
+    gsap.from(".mobile-menu", { duration: 1, ease: "bounce", y: 70 })
+}
+
 
 const steps = ((() => {
     window.addEventListener('DOMContentLoaded', () => { setTimeout(initial, 1) }, true);
@@ -241,16 +249,6 @@ const steps = ((() => {
 })());
 
 
-// const closeBtn = document.getElementById("close-btn-header");
-// closeBtn.addEventListener("click", function () {
-//     let navMenu = document.getElementsByClassName("nav-list-item");
-//     for (navMenuItem of navMenu) {
-//         navMenuItem.classList.toggle("nav-close")
-//     }
-//     let homeIcon = document.querySelector(".burger-home-img");
-//     homeIcon.classList.toggle("rotate1");
-// });
-
 window.addEventListener("DOMContentLoaded", function () {
 
     const closeAnimBtn = document.getElementById("close-btn");
@@ -270,30 +268,9 @@ window.addEventListener("DOMContentLoaded", function () {
     const prouctCards = document.getElementsByClassName("products-item");
     for (productCard of prouctCards) {
         productCard.addEventListener("click", function () {
-            let that = this
-            // if (this.classList.contains("on")) {
-            //     for (card of prouctCards) {
-            //         card.classList.remove("d-none")
-            //     }
-            // }
-            // else {
-            //     setTimeout(function(){
-            //         for (card of prouctCards) {
-            //             card.classList.add("d-none")
-            //         }
-            //     }, 300)
-            // }
             for (let card of prouctCards) {
                 card.classList.toggle("off")
                 card.querySelector(".product-inbox").classList.toggle("off")
-                // if (card.classList.contains("d-none")){
-                //     card.classList.remove("d-none")
-                // }
-                // else {
-                //     setTimeout(function(){
-                //         card.classList.add("d-none")
-                //     }, 300);
-                // }
             }
             inbox = this.querySelector("#outbox")
             if (inbox.classList.contains("inbox-on")) {
@@ -311,7 +288,6 @@ window.addEventListener("DOMContentLoaded", function () {
                 }, 150)
                 inbox.classList.add("inbox-on")
             }
-            // setTimeout(function(){that.classList.remove("d-none");}, 300);
             this.classList.remove("off");
             this.querySelector(".product-inbox").classList.remove("off")
             this.classList.toggle("on");
@@ -319,6 +295,7 @@ window.addEventListener("DOMContentLoaded", function () {
         })
     }
 })
+
 
 
 
@@ -356,7 +333,7 @@ checkboxBox2.addEventListener("click", function () {
 const form2 = document.getElementById("form2")
 const form2btns = document.getElementsByClassName("form2-toggle-btn")
 for (let form2btn of form2btns) {
-    form2btn.addEventListener("click", function(){
+    form2btn.addEventListener("click", function () {
         form2.classList.toggle("form2-active")
         this.querySelector("svg").classList.toggle("rotate45")
     })
@@ -381,89 +358,6 @@ $(window).on("scroll", function () {
 });
 
 
-if (widthWind > 767) {
-    gsap.from("#landing-item", {
-        x: -200,
-        // opacity:0,
-        scrollTrigger: {
-            scrub: 0,
-            start: "-1000px",
-            end: "-250px",
-            trigger: "#landing-item",
-        }
-    })
-
-    gsap.from("#bcard-item", {
-        x: 200,
-        // opacity:0,
-        scrollTrigger: {
-            trigger: "#bcard-item",
-            scrub: 0,
-            start: "-1000px",
-            end: "-300px",
-        }
-    })
-
-    gsap.from("#multi-item", {
-        x: -200,
-        // opacity:0,
-        scrollTrigger: {
-            scrub: 0,
-            start: "-1000px",
-            end: "-450px",
-            trigger: "#multi-item",
-        }
-    })
-
-    gsap.from("#shop-item", {
-        x: 200,
-        // opacity:0,
-        scrollTrigger: {
-            trigger: "#shop-item",
-            scrub: 0,
-            start: "-1000px",
-            end: "-700px",
-        }
-    })
-
-    gsap.from(".other-service", {
-        scale: 0.3,
-        scrollTrigger: {
-            trigger: ".other-services",
-            scrub: true,
-            start: "-1000px",
-            end: "-250px"
-        }
-    })
-
-    gsap.from(".steps-list", {
-        x: -50,
-        opacity: 0,
-        scrollTrigger: {
-            scrub: true,
-            start: "-800px",
-            end: "100%",
-            trigger: "#how-to-work",
-        }
-    })
-
-    gsap.from(".step-icons-box", {
-        x: 50,
-        opacity: 0,
-        scrollTrigger: {
-            trigger: "#how-to-work",
-            scrub: true,
-            start: "-800px",
-            end: "100%",
-        }
-    })
-
-
-}
-
-if (widthWind < 768) {
-    gsap.from(".mobile-menu", { duration: 1, ease: "bounce", y: 70 })
-}
 
 $(document).ready(function () {
     $(document).on("scroll", onScroll);
@@ -485,36 +379,36 @@ function onScroll(event) {
 }
 
 
-$('#footer-num').submit(function(){
-	$.post(
-		'post-email.php', // адрес обработчика
-		 $("#footer-num").serialize(), // отправляемые данные  		
-  
-		function(msg) { // получен ответ сервера  
-			$('#footer-num').hide('slow');
-			$('#message').html(msg);
+$('#footer-num').submit(function () {
+    $.post(
+        'post-email.php', // адрес обработчика
+        $("#footer-num").serialize(), // отправляемые данные  		
+
+        function (msg) { // получен ответ сервера  
+            $('#footer-num').hide('slow');
+            $('#message').html(msg);
             $('#form-title').text('Ваша заявка в пути! Ответим в ближайшее время!')
             $('#form-title').addClass('form-title-agree')
-		}
-	);
-	
-	return false;
+        }
+    );
+
+    return false;
 });
 
-$('#footer-num2').submit(function(){
-	$.post(
-		'post-email.php', // адрес обработчика
-		 $("#footer-num2").serialize(), // отправляемые данные  		
-  
-		function(msg) { // получен ответ сервера  
-			$('#footer-num2').hide('slow');
-			$('#message2').html(msg);
+$('#footer-num2').submit(function () {
+    $.post(
+        'post-email.php', // адрес обработчика
+        $("#footer-num2").serialize(), // отправляемые данные  		
+
+        function (msg) { // получен ответ сервера  
+            $('#footer-num2').hide('slow');
+            $('#message2').html(msg);
             $('#form-title2').text('Ваша заявка в пути! Ответим в ближайшее время!')
             $('#form-title2').addClass('form-title-agree')
-		}
-	);
-	
-	return false;
+        }
+    );
+
+    return false;
 });
 
 
